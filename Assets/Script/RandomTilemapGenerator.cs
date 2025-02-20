75aca323cf9d2eddb4e0bbd9c7ee172691e20ad0
hoa <hoaphung10112003@gmail.com>
2025-02-20 15:29:56 +0700

    a

---
+++ Assets/Script/RandomTilemapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomTilemapGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase[] tiles; // Mảng chứa các tile (hình nền)
+
+    public int width = 10;  // Số ô ngang
+    public int height = 10; // Số ô dọc
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                TileBase randomTile = tiles[Random.Range(0, tiles.Length)];
+                tilemap.SetTile(new Vector3Int(x, y, 0), randomTile);
+            }
+        }
+    }
+}
