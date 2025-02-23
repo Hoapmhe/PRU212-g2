c44c8129d66fcf7c28aa7ecd10b2402d411ada43
HoangQuan425 <quanqhhq@gmail.com>
2025-02-23 20:20:37 +0700

    update random package

--- Assets/Script/ObstacleSpawner.cs
+++ Assets/Script/ObstacleSpawner.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-	public GameObject obstaclePrefab;     // Prefab chướng ngại vật
-	public Transform[] roadPoints;         // Mảng các điểm trên đường
-	public int numberOfObstacles = 5;     // Số lượng chướng ngại vật
+	public GameObject obstaclePrefab;     
+	public Transform[] roadPoints;        
+	public int numberOfObstacles = 5;     
 
 	void Start()
 	{
@@ -14,35 +14,29 @@ public class ObstacleSpawner : MonoBehaviour
 
 	public void SpawnObstacles()
 	{
-		// Xóa obstacles cũ
 		GameObject[] oldObstacles = GameObject.FindGameObjectsWithTag("Obstacle");
 		foreach (GameObject obstacle in oldObstacles)
 		{
 			Destroy(obstacle);
 		}
 
-		// Tạo list các vị trí có thể spawn
 		List<int> availablePositions = new List<int>();
 		for (int i = 0; i < roadPoints.Length; i++)
 		{
 			availablePositions.Add(i);
 		}
 
-		// Spawn obstacles tại các điểm ngẫu nhiên trên đường
 		for (int i = 0; i < Mathf.Min(numberOfObstacles, roadPoints.Length); i++)
 		{
 			if (availablePositions.Count == 0) break;
 
-			// Chọn ngẫu nhiên một vị trí từ các vị trí còn lại
 			int randomIndex = Random.Range(0, availablePositions.Count);
 			int positionIndex = availablePositions[randomIndex];
 
-			// Spawn obstacle tại vị trí đã chọn
 			Vector3 spawnPosition = roadPoints[positionIndex].position;
 			GameObject newObstacle = Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
 			newObstacle.tag = "Obstacle";
 
-			// Xóa vị trí đã sử dụng
 			availablePositions.RemoveAt(randomIndex);
 		}
 	}
---
+++ Assets/Script/PackageSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageSpawner : MonoBehaviour
+{
+	public GameObject packagePrefab;
+	public Transform[] spawnPoints;
+	public int numberOfPackages = 5;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		SpawnPackages();
+	}
+	public void SpawnPackages()
+	{
+		// Xóa tất cả package cũ
+		GameObject[] oldPackages = GameObject.FindGameObjectsWithTag("Package");
+		foreach (GameObject package in oldPackages)
+		{
+			Destroy(package);
+		}
+
+		// Tạo danh sách các vị trí có thể spawn
+		List<int> availablePositions = new List<int>();
+		for (int i = 0; i < spawnPoints.Length; i++)
+		{
+			availablePositions.Add(i);
+		}
+
+		// Spawn packages tại các điểm ngẫu nhiên
+		for (int i = 0; i < Mathf.Min(numberOfPackages, spawnPoints.Length); i++)
+		{
+			if (availablePositions.Count == 0) break;
+
+			// Chọn ngẫu nhiên một vị trí
+			int randomIndex = Random.Range(0, availablePositions.Count);
+			int positionIndex = availablePositions[randomIndex];
+
+			// Spawn package
+			Vector3 spawnPosition = spawnPoints[positionIndex].position;
+			GameObject newPackage = Instantiate(packagePrefab, spawnPosition, Quaternion.identity);
+			newPackage.tag = "Package";
+
+			// Xóa vị trí đã dùng
+			availablePositions.RemoveAt(randomIndex);
+		}
+	}
+
+}
