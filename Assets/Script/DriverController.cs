c8394d0d3e8e72b81dd1223dbf926972d7f6420e
Zeny <HoangAnh@23>
2025-02-23 14:54:42 +0700

    Take Package and Bring Location

--- Assets/Script/DriverController.cs
+++ Assets/Script/DriverController.cs
@@ -1,12 +1,17 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DriverController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] float steerSpeed = 30f;
+    [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float minSpeed = 2f;
+    [SerializeField] private float speedDecrease = 1f;
+    [SerializeField] private float steerSpeed = 30f;
+
+    private float currentSpeed;
+
     void Start()
     {
-
+        currentSpeed = baseSpeed;
     }
 
     void Update()
@@ -17,8 +22,18 @@ public class DriverController : MonoBehaviour
     void HandleMoving()
     {
         float changeSteer = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float changeMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float changeMove = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
         transform.Translate(0, changeMove, 0);
         transform.Rotate(0, 0, -changeSteer);
+    }
+
+    public void DecreaseSpeed()
+    {
+        currentSpeed = Mathf.Max(minSpeed, currentSpeed - speedDecrease); // Đảm bảo tốc độ không giảm xuống dưới minSpeed
+    }
+
+    public void ResetSpeed()
+    {
+        currentSpeed = baseSpeed; // Khôi phục tốc độ ban đầu khi giao hàng
     }
 }
