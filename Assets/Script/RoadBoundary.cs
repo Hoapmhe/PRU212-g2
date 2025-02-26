6d010fdcaa6a9326ea198af65705c54a81897323
HoangQuan425 <quanqhhq@gmail.com>
2025-02-27 00:00:48 +0700

    update road

---
+++ Assets/Script/RoadBoundary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RoadBoundary : MonoBehaviour
+{
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		DriverController driver = collision.gameObject.GetComponent<DriverController>();
+		if (driver != null)
+		{
+			Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+			if (rb != null)
+			{
+				Vector2 bounceDirection = collision.contacts[0].normal; // Hướng phản lực từ điểm va chạm
+				rb.AddForce(bounceDirection * 10f, ForceMode2D.Impulse); // Điều chỉnh lực đẩy
+			}
+		}
+	}
+}
