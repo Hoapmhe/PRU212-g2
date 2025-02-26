1f8d658ff03d0911bd82ede8a00b508832d5b563
hoa <hoaphung10112003@gmail.com>
2025-02-26 22:54:12 +0700

    mini map

---
+++ Assets/Script/MiniMapFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MiniMapFollow2D : MonoBehaviour
+{
+    [SerializeField] public Transform target; // Xe cần theo dõi
+    public Vector3 offset = new Vector3(0f, 10f, -10f); // Độ cao của Camera so với xe
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        // Cập nhật vị trí của MiniMapCamera theo xe, giữ nguyên Z
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+    }
+}
